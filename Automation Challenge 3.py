044d40406317c96654d7d2e8b4cd2c538c29cadb
zakgdata <32788833+zakgdata@users.noreply.github.com>
2020-05-05 19:15:09 -0400

    Add files via upload

---
+++ Automation Challenge 3.py
@@ -0,0 +1,55 @@
+import pandas as pd
+import logging
+import requests
+import tableauserverclient as TSC
+import smtplib
+
+tableau_auth = TSC.TableauAuth('zak.g.data@gmail.com', '', 'betatestingfortableauserver')
+server = TSC.Server('https://10ax.online.tableau.com')
+logging.basicConfig(level='INFO')
+server.version = '3.7'
+
+def main():
+    #TABLEAU METADATA QUERY
+    query = """
+        {
+        fields{
+            name
+            datasource{
+                downstreamOwners{
+                    email
+                }
+            }
+        }
+    }
+    """
+    #AUTHENTICATE WITH TABLEAU SERVER
+    with server.auth.sign_in(tableau_auth):
+        resp = server.metadata.query(query)
+        datasources = resp['data']
+    
+    #CONVERT DATA INTO DATAFRAME
+    db_servers_df = pd.DataFrame(datasources['fields'])
+    b = db_servers_df[db_servers_df['name'].isin(['Calculation1', 'Calculation2', 'Calculation3', 'Calculation4', 'Calculation5', 'Calculation6', 'Calculation7', 'Calculation8', 'Calculation9', 'Calculation10',
+        'Test1', 'Test2', 'Test3', 'Test4', 'Test5', 'Test6', 'Test7', 'Test8', 'Test9', 'Test10'])]
+    b['datasource'] = b['datasource'].astype(str).str.replace("\{\'downstreamOwners\'\: \[\{\'email\'\: \'", '')
+    b['datasource'] = b['datasource'].astype(str).str.replace("\'\}\]\}", '')
+    b = b.rename(columns={"name": "Field", "datasource": "Email"})
+    
+    #GENERATE EMAIL TO ASSOCIATED EMAIL
+    i=0    
+    for row in b:
+        with smtplib.SMTP('smtp.gmail.com', 587) as smtp:
+            smtp.ehlo()
+            smtp.starttls()
+            smtp.ehlo()
+            smtp.login('zak.g.data@gmail.com', '')
+            subject = 'Poorly Named Field - ' + b.iat[i,0]
+            body = 'Hello!\n\nPlease consider updating this field name to something more fitting: {}\n\nThanks, \nYour friendly neighborhood Tableau Admin'.format(b.iat[i,0])
+            msg = f'Subject: {subject}\n\n{body}'
+            smtp.sendmail('zak.g.data@gmail.com', b.iat[i,1], msg)
+            i += 1
+
+            
+if __name__ == '__main__':
+    main()
